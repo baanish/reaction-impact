731155b358171a7e7454339a761166601fd60de3
abhirud <aanishbhirud@gmail.com>
2022-08-29 17:41:41 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,5 @@ export default defineConfig({
     // Enable React for the Algolia search component.
     react(),
   ],
-  site: `https://baanish.github.io`,
+  site: `https://baanish.github.io/reaction-impact`,
 });
